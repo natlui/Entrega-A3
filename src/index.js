36f324ba8aa8d36f6b91fe0abbfd8bb4e6c75823
Julio Dantas <Julio_dantas.10@hotmail.com>
2023-10-05 10:31:47 -0300

    projetoA3(git-react): Esqueleto react com npx

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const App = () =>{
+    return (
+<div style={{margin: 'auto', width:1000, backgroundColor: '#EEE', padding: 18, borderRadius:10}}>
+    <label htmlFor="Descarte" style={{ display:'block', marginBottom: 1, fontFamily:'Monospace', textAlign: 'center', fontSize:'30px'}}>
+    Descarte de alimentos não fiscalizado!!
+    </label>
+
+
+
+</div>
+
+
+    )
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
